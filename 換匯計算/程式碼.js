f068299395720f07cdcfdcb64d57556e6114966e
kevin823lin <kevin823lin@gmail.com>
2020-11-30 01:26:29 +0800

    修改備註與排版

--- 換匯計算/程式碼.js
+++ 換匯計算/程式碼.js
@@ -1,17 +1,16 @@
-var SpreadSheet = SpreadsheetApp.getActiveSpreadsheet();
-var defaultSheet = SpreadSheet.getSheetByName("default");
+var SpreadSheet = SpreadsheetApp.getActiveSpreadsheet(); //取得試算表
+var defaultSheet = SpreadSheet.getSheetByName("default"); //取得工作表
 
 function myFunction() {
-  
+    
 }
 
-var minimum = 100;
-var smallest_unit = 2;
+var minimum = 100; //設定最小換匯金額
+var smallest_unit = 2; //設定最小換匯單位
 
 function ask(a) { //台幣換外幣-賣匯
     var min = 999, str = "";
-    for (var num = Math.floor(Math.floor(minimum / a * 20) / 20); num <= Math.floor(Math.floor(minimum / a * 20) / 20.0) + 50; num += Math.pow(10, -smallest_unit))
-    {
+    for (var num = Math.floor(Math.floor(minimum / a * 20) / 20); num <= Math.floor(Math.floor(minimum / a * 20) / 20.0) + 50; num += Math.pow(10, -smallest_unit)) {
         var temp = a * num;
         if (temp < minimum - 0.5) continue;
         var temp2 = Math.floor(temp);
@@ -21,15 +20,14 @@ function ask(a) { //台幣換外幣-賣匯
             str += newLine(temp.toFixed(5), num.toFixed(smallest_unit), min.toFixed(5));
         }
     }
-    str = str.slice(0,str.length-1)
+    str = str.slice(0, str.length - 1);
     Logger.log(str);
     return str;
 }
 
 function bid(b) { //外幣換台幣-買匯
     var max = -1, str = "";
-    for (var num = Math.floor(Math.floor(minimum / b * 20) / 20); num <= Math.floor(Math.floor(minimum / b * 20) / 20.0) + 50; num += Math.pow(10, -smallest_unit))
-    {
+    for (var num = Math.floor(Math.floor(minimum / b * 20) / 20); num <= Math.floor(Math.floor(minimum / b * 20) / 20.0) + 50; num += Math.pow(10, -smallest_unit)) {
         var temp = b * num;
         if (temp < minimum - 0.5) continue;
         var temp2 = Math.floor(temp) + 1;
@@ -39,23 +37,21 @@ function bid(b) { //外幣換台幣-買匯
             str += newLine(temp.toFixed(5), num.toFixed(smallest_unit), max.toFixed(5));
         }
     }
-    str = str.slice(0,str.length-1)
+    str = str.slice(0, str.length - 1);
     Logger.log(str);
     return str;
 }
 
-function newLine(...text){
+function newLine(...text) { //將傳入的變數以空格隔開並換行
     var str = ""
-    text.forEach(function(t)
-        {
-            str += t + " "
-        }
-    )
-    str = str.slice(0,str.length-1) + "\n";
+    text.forEach(function (t) {
+        str += t + " "
+    })
+    str = str.slice(0, str.length - 1) + "\n";
     return str;
 }
 
-function doPost(e) {
+function doPost(e) { //處理 LineBot Post Request
     var param = e.postData.contents;
     Logger.log(param);
     var json = JSON.parse(param).events;
@@ -78,9 +74,9 @@ function doPost(e) {
     return;
 }
 
-Auth = defaultSheet.getRange("C2").getValues();
+Auth = defaultSheet.getRange("C2").getValues(); //從試算表取得 LineBot Auth
 
-function LineBotReply(str, replyToken) {
+function LineBotReply(str, replyToken) { //回覆 Line Bot 訊息
     url = "https://api.line.me/v2/bot/message/reply";
     var options = {
         "async": true,
